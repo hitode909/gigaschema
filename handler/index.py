4a66effb90cd49624c160c491726a94bf0f6f958
hakobe932 <hakobe@gmail.com>
2010-11-27 13:14:58 +0900

    api_keyをつくるようにしたりとか

--- handler/index.py
+++ handler/index.py
@@ -1,9 +1,13 @@
+# :set encoding=utf-8
 import os
 import logging
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util, template
 from datetime import datetime
 from helper import *
+from model import *
+from time import time
+import hashlib
 
 class IndexHandler(webapp.RequestHandler):
     def get(self):
@@ -13,4 +17,32 @@ class IndexHandler(webapp.RequestHandler):
         self.response.out.write(ViewHelper.process('index', template_values))
 
     def post(self):
-        self.response.out.write(self.request.get('name') + " was created")
+        name = self.request.get('name')
+        origin = self.request.get('origin')
+        rule = self.request.get('rule')
+
+        # このへんでバリデーション
+
+        s = hashlib.sha1()
+        s.update(str(time()))
+        s.update(name.encode('utf-8'))
+        s.update('user_id')
+        api_key = s.hexdigest()
+
+        schema = Schema(
+            name=name, 
+            origin=origin,
+            api_key=api_key,
+            rule=rule
+        )
+        schema.put()
+
+        template_values = {
+            "name": name,
+            "origin": origin,
+            "api_key": api_key,
+            "rule": rule,
+        }
+
+        # スキーマページにリダイレクト
+        self.response.out.write(ViewHelper.process('index', template_values))
