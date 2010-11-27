60ac890a97838e0092b8075510599298a321492f
hitode909 <hitode909@gmail.com>
2010-11-27 18:06:17 +0900

    data page

--- handler/data.py
+++ handler/data.py
@@ -1,17 +1,30 @@
 import os
 import logging
-from google.appengine.ext import webapp
+from google.appengine.ext import webapp, db
 from google.appengine.ext.webapp import util, template
 from datetime import datetime
 from helper import *
+from model import *
 
 class DataHandler(webapp.RequestHandler):
     def get(self, owner_name, schema_name, data_key):
+        schema = Schema.retrieve_by_names(owner_name, schema_name)
+        if not schema:
+            logging.info("schema not found " + schema_name)
+            self.response.out.write(self.response.http_status_message(404))
+            return
+
+        data = Data.get(data_key)
+        if not data:
+            logging.info("data not found " + data_key)
+            self.response.out.write(self.response.http_status_message(404))
+            return
+
         template_values = {
             'owner_name': owner_name,
             'schema_name': schema_name,
             'data_key': data_key,
-            'data': None,       # TODO
+            'data': data,       # TODO
             'data_url': self.request.path # TODO = data.url
             }
         self.response.out.write(ViewHelper.process('data', template_values))
