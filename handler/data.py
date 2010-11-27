80001f8712cc06fba0930ce105d1df4f2bae9c2c
hitode909 <hitode909@gmail.com>
2010-11-27 14:25:44 +0900

    data delete

--- handler/data.py
+++ handler/data.py
@@ -11,5 +11,17 @@ class DataHandler(webapp.RequestHandler):
             'owner_name': owner_name,
             'schema_name': schema_name,
             'data_key': data_key,
+            'data': None,       # TODO
+            'data_url': self.request.path # TODO = data.url
             }
         self.response.out.write(ViewHelper.process('data', template_values))
+
+    def delete(self, owner_name, schema_name, data_key):
+        self.response.out.write(data_key + " deleted")
+
+    def post(self, owner_name, schema_name, data_key):
+        if not self.request.get('delete'):
+            self.response.out.write(self.response.http_status_message(400))
+            return
+
+        return self.delete(owner_name, schema_name, data_key)
