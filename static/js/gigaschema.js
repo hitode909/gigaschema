bf94f4ab554d563d9e1354a50987a088ee870f14
hitode909 <hitode909@gmail.com>
2010-11-27 17:21:07 +0900

    graph plot

--- static/js/gigaschema.js
+++ static/js/gigaschema.js
@@ -1,10 +1,12 @@
-function dispatcher (guard, func) {
-    dispatcher.path_func = dispatcher.path_func || []
+window.gigaschema = {  };
+
+window.gigaschema.dispatcher = function(guard, func) {
+    window.gigaschema.dispatcher.path_func = window.gigaschema.dispatcher.path_func || []
     if (func) {
-        dispatcher.path_func.push([guard, func]);
+        window.gigaschema.dispatcher.path_func.push([guard, func]);
         return;
     }
-    dispatcher.path_func.forEach(function(pair) {
+    window.gigaschema.dispatcher.path_func.forEach(function(pair) {
         var guard = pair[0];
         var func = pair[1];
 
@@ -13,18 +15,29 @@ function dispatcher (guard, func) {
     });
 };
 
-dispatcher('body#schema', function() {
+// [['2009/07/01 18:00:00',2], ['2009/09/08 18:30:00',11], ['2009/09/08 18:40:00',15], ['2010/01/02',3],['2010/01/03',6],['2010/02/01',3]]
+window.gigaschema.plotGraph = function(id, title, data) {
     $.jqplot('chartdiv',
-             [[['2009/07/01 18:00:00',2], ['2009/09/08 18:30:00',11], ['2009/09/08 18:40:00',15], ['2010/01/02',3],['2010/01/03',6],['2010/02/01',3]]], {
-                 title:'雪が降ってうれしい',
+             data, {
+                 title:title,
                  axes: {
                      xaxis: {
                          renderer: $.jqplot.DateAxisRenderer
                      }
                  }
              });
+}
+
+window.gigaschema.dispatcher('body#schema', function() {
+    var from = window.gigaschema.currentSchema;
+    var to = [];
+    from.data.forEach(function(row) {
+        // TODO: validation
+        to.push([row.timestamp, parseInt(row.value, 10)]);
+    });
+    window.gigaschema.plotGraph('chartdiv', from.name, [to]);
 });
 
 $(function() {
-    dispatcher();
+    window.gigaschema.dispatcher();
 });
