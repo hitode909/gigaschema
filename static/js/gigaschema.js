8c8d1dc391d6d7e0a9d683d5e0b51b1ad935f51d
hitode909 <hitode909@gmail.com>
2010-11-27 16:10:46 +0900

    graph with date

--- static/js/gigaschema.js
+++ static/js/gigaschema.js
@@ -15,10 +15,13 @@ function dispatcher (guard, func) {
 
 dispatcher('body#schema', function() {
     $.jqplot('chartdiv',
-             [[[1, 2],[3,5.12],[5,13.1],[7,33.6],[9,85.9],[11,219.9]]],
-             { title:'Exponential Line',
-               axes:{yaxis:{min:-10, max:240}},
-               series:[{color:'#5FAB78'}]
+             [[['2009/07/01 18:00:00',2], ['2009/09/08 18:30:00',11], ['2009/09/08 18:40:00',15], ['2010/01/02',3],['2010/01/03',6],['2010/02/01',3]]], {
+                 title:'雪が降ってうれしい',
+                 axes: {
+                     xaxis: {
+                         renderer: $.jqplot.DateAxisRenderer
+                     }
+                 }
              });
 });
 
