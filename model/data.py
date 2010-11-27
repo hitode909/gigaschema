dcf83e0e133ae18aa90150055f30c173b442ba41
hitode909 <hitode909@gmail.com>
2010-11-27 21:31:08 +0900

    data_item template

--- model/data.py
+++ model/data.py
@@ -23,3 +23,32 @@ class Data(db.Model):
             'timestamp': str(self.created_on),
             'value': self.value,
         }
+
+
+    def item_type(self):
+        r_img = re.compile('^data:image/')
+        r_audio = re.compile('^data:audio/')
+
+        if r_img.match(self.value):
+            return 'image'
+        if r_audio.match(self.value):
+            return 'audio'
+
+        try:
+            float(self.value)
+            return 'number'
+        except ValueError:
+            return 'text'
+
+    def is_text_item(self):
+        return self.item_type() == "text"
+
+    def is_number_item(self):
+        return self.item_type() == "number"
+
+    def is_image_item(self):
+        return self.item_type() == "image"
+
+    def is_audio_item(self):
+        return self.item_type() == "audio"
+
