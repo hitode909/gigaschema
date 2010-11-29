5398e77286fe984c4ea403807c93ae77531bbd65
hitode909 <hitode909@gmail.com>
2010-11-29 12:20:14 +0900

    item_typeをプロパティにした

--- model/data.py
+++ model/data.py
@@ -2,12 +2,14 @@ from google.appengine.ext import db
 from model.schema import Schema
 import re
 import datetime
+import logging
 
 class Data(db.Model):
     schema = db.ReferenceProperty(Schema, required=True)
     created_on = db.DateTimeProperty(auto_now=True)
     group = db.StringProperty()
     value = db.TextProperty()
+    item_type = db.TextProperty()
 
     @classmethod
     def create(klass, schema=None, group=None, value=None):
@@ -17,6 +19,7 @@ class Data(db.Model):
             value=value,
         )
         data.put()
+        data.set_item_type()
         return data
 
     @classmethod
@@ -35,6 +38,7 @@ class Data(db.Model):
                 created_on = now,
             )
             data.put()
+            data.set_item_type()
             results.append(data)
             now = now.replace(microsecond = now.microsecond+1)
         return results
@@ -50,54 +54,60 @@ class Data(db.Model):
             'timestamp': str(self.created_on),
             'group': self.group,
             'value': self.value,
+            'item_type': self.item_type,
         }
 
-    def item_type(self):
-        if self.item_type_cache:
-            return self.item_type_cache
+    def set_item_type(self):
+        if self.item_type:
+            return
 
-        self.item_type_cache = self.get_item_type()
-        return self.item_type_cache
+        self.item_type = self.get_item_type()
+        self.put()
 
     def get_item_type(self):
+        value = self.value
+
         r_url = re.compile('^(https?)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)$')
         r_url_img = re.compile('jpg|png|gif|bmp$', re.IGNORECASE)
         r_url_audio = re.compile('wav|mp3|m4a$', re.IGNORECASE)
         r_img = re.compile('^data:image/', re.IGNORECASE)
         r_audio = re.compile('^data:audio/', re.IGNORECASE)
 
-        if r_img.match(self.value):
+        if r_img.match(value):
             return 'image'
-        if r_audio.match(self.value):
+        if r_audio.match(value):
             return 'audio'
 
-        if r_url.match(self.value):
-            if r_url_img.search(self.value):
+        if r_url.match(value):
+            if r_url_img.search(value):
                 return 'image'
-            if r_url_audio.search(self.value):
+            if r_url_audio.search(value):
                 return 'audio'
             return 'url'
 
         try:
-            float(self.value)
+            float(value)
             return 'number'
         except ValueError:
             return 'text'
 
     def is_text_item(self):
-        return self.item_type() == 'text'
+        if not self.item_type:     # XXX
+            self.set_item_type()   # XXX
+
+        return self.item_type == 'text'
 
     def is_number_item(self):
-        return self.item_type() == 'number'
+        return self.item_type == 'number'
 
     def is_image_item(self):
-        return self.item_type() == 'image'
+        return self.item_type == 'image'
 
     def is_audio_item(self):
-        return self.item_type() == 'audio'
+        return self.item_type == 'audio'
 
     def is_url_item(self):
-        return self.item_type() == 'url'
+        return self.item_type == 'url'
 
     @classmethod
     def validate_group(klass, value):
