e387dfbcf2163875ac6401e561982d38fe2ed628
hitode909 <hitode909@gmail.com>
2010-12-04 16:36:40 +0900

    catch

--- static/js/gigaschema.js
+++ static/js/gigaschema.js
@@ -83,3 +83,68 @@ $(function() {
     window.gigaschema.dispatcher();
 });
 
+Deferred.define();
+
+google.load("search", "1");
+google.setOnLoadCallback(function() {
+    if ($('body#index').length == 0) return;
+
+    function getImageUrl(keyword, d) {
+        var search = new google.search.SearchControl();
+        search.setResultSetSize(google.search.Search.LARGE_RESULTSET);
+        search.addSearcher(new google.search.ImageSearch());
+        search.draw();
+
+        var deferred = new Deferred();
+        search.setSearchCompleteCallback(this, function(sc, searcher) {
+            var result = searcher.results[Math.floor(Math.random() * searcher.results.length)];
+            deferred.call(result.url);
+        });
+
+        search.execute(keyword);
+	return deferred;
+    };
+
+    // http://hail2u.net/blog/coding/shuffle-array-in-javascript.html
+    function shuffle(list) {
+        var i = list.length;
+
+        while (--i) {
+            var j = Math.floor(Math.random() * (i + 1));
+            if (i == j) continue;
+            var k = list[i];
+            list[i] = list[j];
+            list[j] = k;
+        }
+
+        return list;
+    }
+
+    $.getJSON('/test/keyword.json', function(schema) {
+        var keywords = shuffle(schema.data).map(function(data) { return data.value });
+
+        results = [];
+        loop(3, function (i) {
+            return getImageUrl(keywords[i]).next(function(url) {
+                results.push([keywords[i], url]);
+            });
+        }).
+            next(function () {
+                var copy = [];
+                results.forEach(function(pair) {
+                    var keyword = pair[0];
+                    var url = pair[1];
+                    var img = $('<img>');
+                    img.attr( {src: url });
+                    $('#welcome').append(img);
+                    copy.push(keyword);
+                });
+                $('#welcome').css({opacity: 0.1}).animate({opacity: 1.0}, 1000);
+                $('#welcome #copy').text(copy.join('、') + '。');
+            });
+
+    });
+
+});
+
+
