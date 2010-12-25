15f72381085bc477937968d1bfa79be85079b8af
hitode909 <hitode909@gmail.com>
2010-12-25 21:03:34 +0900

    user feed

--- handler/user.py
+++ handler/user.py
@@ -3,10 +3,12 @@ import os
 import logging
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util, template
+from google.appengine.api import memcache
 from helper import *
 from model import *
 from handler.base import BaseHandler
 from handler.base import hook_request
+from django.utils import feedgenerator
 
 class UserHandler(BaseHandler):
 
@@ -49,3 +51,39 @@ class UserRedirectHandler(BaseHandler):
             self.error_response(403, log_msg="please login")
 
         self.redirect('/' + UserHelper.extract_user_name(self.stash['user']))
+
+class UserFeedHandler(BaseHandler):
+    @hook_request
+    def get(self, owner_name):
+        owner = UserHelper.constract_user(owner_name)
+        if not owner_name:
+            self.error_response(404, log_msg="user not found")
+
+        user_feed_key = '/'.join(['schema', owner_name, 'feed']);
+        user_feed = memcache.get(user_feed_key)
+
+        if user_feed:
+            logging.info('cache hit(schema.feed): ' + user_feed_key)
+        else:
+            data_list = Data.all().filter('is_deleted =', False).filter('owner = ', owner).order('-created_on').fetch(20, 0)
+
+            feed = feedgenerator.Atom1Feed(
+                title = owner_name + ' - GIGA SCHEMA',
+                link = 'http://gigaschema.appspot.com/' + owner_name,
+                description = "",
+                language = 'ja',
+                author_name = owner_name,
+            )
+
+            for data in data_list:
+                feed.add_item(
+                    title =('/'.join([owner_name, data.schema.name, str(data.key())])) + ' - GIGA SCHEMA',
+                    unique_id = '/'.join([owner_name, data.schema.name, str(data.key())]),
+                    link = 'http://gigaschema.appspot.com' + data.url(),
+                    description = data.value_as_html(),
+                    pubdate = data.created_on,
+                )
+            user_feed = feed.writeString('utf-8')
+            memcache.set(key=user_feed_key, value=user_feed, time=60*30)
+        self.response.headers['Content-Type'] = 'application/atom+xml;type=feed;charset="utf-8"'
+        self.response.out.write(user_feed)
